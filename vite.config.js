acb198b6cc9b99f16dd67a33dc390b9ea043135a
《ABB》 <142119322+kenny23599@users.noreply.github.com>
2025-12-08 18:09:13 +0100

    Refactor Vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5000,
-    open: true
-  }
-})
+export default {
+  base: 'EasyLearn', // if deploying to GitHub Pages
+}
