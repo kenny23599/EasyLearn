3a8d90f4d97d2a47e1acc40a2d30def248d0399c
《ABB》 <142119322+kenny23599@users.noreply.github.com>
2025-12-09 06:10:42 +0100

    Wrap App component with Router and StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/globals.css'
+import { BrowserRouter as Router } from 'react-router-dom';
 
+// Wrap your app with Router
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
