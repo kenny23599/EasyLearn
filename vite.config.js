6132ae934d6ba432561817817374a8b8595d641f
《ABB》 <142119322+kenny23599@users.noreply.github.com>
2025-12-09 06:21:50 +0100

    Refactor Vite config to use defineConfig

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
-  base: 'EasyLearn', // if deploying to GitHub Pages
-}
+export default defineConfig({
+  plugins: [react()],
+  base: '/EasyLearn/', // Add this line
+})
