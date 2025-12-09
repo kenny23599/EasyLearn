1a09ecc5bca3e217b2b8776c993d00ab5897af32
《ABB》 <142119322+kenny23599@users.noreply.github.com>
2025-12-09 06:03:11 +0100

    Remove HashRouter and StrictMode from rendering

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/globals.css'
 
-import { HashRouter } from 'react-router-dom';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-);
+  <App />
+)
